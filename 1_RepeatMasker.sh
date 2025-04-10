aed95ec4a60410c02a7428c7eaa4fa4580da5764
drabe004 <63505920+drabe004@users.noreply.github.com>
2025-04-09 21:50:00 -0500

    Update 1_RepeatMasker.sh

--- 1_RepeatMasker.sh
+++ 1_RepeatMasker.sh
@@ -15,11 +15,11 @@ module load repeatmodeler
 
 cd /path/to/your/genomes
 
-#Step 1: Build a database for the genome (in the example below the genome fasta file is PfluvDB GCA_010015445.1_GENO_Pfluv_1.0_genomic.fa and I chose the prefix PfluvDB for the database files)
-#BuildDatabase -name MBRIDB MBRI_genome.fasta
+#Step 1: Build a database for the genome (in the example below the genome fasta file is CAVEgenome.fa and I chose the prefix CAVEDB for the database files) 
+BuildDatabase -name CAVEDB CAVEgenome.fa
 
-#Step 2: Use the database to build a custon repeat library
-#RepeatModeler -database MBRIDB -pa 100
+#Step 2: Use the database to build a custon repeat library 
+RepeatModeler -database CAVEDB -pa 100
 
-#Step 3: Run RepeatMasker; I think you want to include the flag -xsmall to get a softmaksed genome as the output (check options with RepeatMasker -h after calling module load repeatmasker/4.0.5)
-RepeatMasker -xsmall -pa 24 -lib ./RM_398182.FriJul91606562021/consensi.fa.classified -dir MBRI_masked_out MBRIDB MBRI_genome.fasta
+#Step 3: Run RepeatMasker; Include the flag -xsmall to get a softmaksed genome as the output (repeatmasker/4.0.5) 
+RepeatMasker -xsmall -pa 24 -lib ./CAVEDB/consensi.fa.classified -dir CAVEDB_masked_out CAVEDB CAVEgenome.fa
