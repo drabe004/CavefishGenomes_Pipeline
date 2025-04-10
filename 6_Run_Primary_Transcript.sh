2388dba9d4deae15b5ea7fb9e4930a3277cba6ae
drabe004 <63505920+drabe004@users.noreply.github.com>
2025-04-10 13:20:46 -0500

    Update and rename Run_Primary_Transcript.sh to 6_Run_Primary_Transcript.sh

---
+++ 6_Run_Primary_Transcript.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l        
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=2
+#SBATCH --mem=50g
+#SBATCH --tmp=10g
+#SBATCH --mail-type=ALL  
+#SBATCH --mail-user=youremail
+
+
+
+
+module load python
+module load orthofinder
+
+#Path to all proteome files .pep.all.fa or GFC*_*_protein.fa
+cd path/to/your/MASTER_FISH_PROTS
+
+python
+
+for f in *fa ; do python path/to/primary_transcript.py $f ; done
+
+#output makes a folder of Primary Transcripts "primary_transcripts"
